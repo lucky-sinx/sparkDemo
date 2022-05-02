9bca7221b90013f1f0624c18c4c14bba71a8d02e
wangyu <2549261744@qq.com>
2022-05-02 22:22:55 +0800

    记录状态的操作：updateStateByKey

---
+++ study/src/main/java/sparkStreaming/SparkStreaming5_State.java
@@ -0,0 +1,52 @@
+package sparkStreaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SparkStreaming5_State {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("spark streaming");
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, new Duration(3000));
+
+        //存缓冲区的路径
+        jsc.checkpoint("cp");
+
+        JavaReceiverInputDStream<String> datas = jsc.socketTextStream("localhost", 9999);
+
+        //无状态
+        //JavaPairDStream<String, Integer> wc = datas.flatMap(line -> Arrays.stream(line.split(" ")).iterator())
+        //        .mapToPair(word -> new Tuple2<>(word, 1))
+        //        .reduceByKey((x, y) -> x + y);
+
+        //有状态,map操作可以照常进行，但是reduce需要updateStata
+        JavaPairDStream<String, Integer> wto1 = datas.flatMap(line -> Arrays.stream(line.split(" ")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1));
+
+        JavaPairDStream<String, Integer> wc = wto1.updateStateByKey(new Function2<List<Integer>, Optional<Integer>, Optional<Integer>>() {
+            @Override
+            public Optional<Integer> call(List<Integer> integers, Optional<Integer> integerOptional) throws Exception {
+                int res = integerOptional.orNull() == null ? 0 : integerOptional.get();
+                for (int num : integers) res += num;
+                return Optional.of(res);
+            }
+        });
+
+        wc.print();
+
+        jsc.start();
+        try {
+            jsc.awaitTermination();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
