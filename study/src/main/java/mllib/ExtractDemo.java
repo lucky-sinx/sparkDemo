c5cccd760c944e9ec8c2a9053f6962074589e20e
wangyu <2549261744@qq.com>
2022-05-06 16:46:41 +0800

    extractor

---
+++ study/src/main/java/mllib/ExtractDemo.java
@@ -0,0 +1,80 @@
+package mllib;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.ml.feature.CountVectorizer;
+import org.apache.spark.ml.feature.CountVectorizerModel;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.ml.feature.Word2Vec;
+import org.apache.spark.ml.feature.Word2VecModel;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
+public class ExtractDemo {
+    static SparkConf conf = new SparkConf().setMaster("local").setAppName("my app");
+    static SparkSession spark = SparkSession.builder()
+            .appName("sql app")
+            .config(conf)
+            .getOrCreate();
+    @Test
+    public void test_word2vec(){
+        // Input data: Each row is a bag of words from a sentence or document.
+        List<Row> data = Arrays.asList(
+                RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
+                RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
+                RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" ")))
+        );
+        StructType schema = new StructType(new StructField[]{
+                new StructField("text", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
+        });
+        Dataset<Row> documentDF = spark.createDataFrame(data, schema);
+
+// Learn a mapping from words to Vectors.
+        Word2Vec word2Vec = new Word2Vec()
+                .setInputCol("text")
+                .setOutputCol("result")
+                .setVectorSize(3)
+                .setMinCount(0);
+
+        Word2VecModel model = word2Vec.fit(documentDF);
+        Dataset<Row> result = model.transform(documentDF);
+
+        for (Row row : result.collectAsList()) {
+            List<String> text = row.getList(0);
+            Vector vector = (Vector) row.get(1);
+            System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
+        }
+    }
+    @Test
+    public void test_countVectorizer(){
+        // Input data: Each row is a bag of words from a sentence or document.
+        List<Row> data = Arrays.asList(
+                RowFactory.create(Arrays.asList("a", "b", "c")),
+                RowFactory.create(Arrays.asList("a", "b", "b", "c", "a"))
+        );
+        StructType schema = new StructType(new StructField [] {
+                new StructField("text", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
+        });
+        Dataset<Row> df = spark.createDataFrame(data, schema);
+
+// fit a CountVectorizerModel from the corpus
+        CountVectorizerModel cvModel = new CountVectorizer()
+                .setInputCol("text")
+                .setOutputCol("feature")
+                .setVocabSize(3)
+                .setMinDF(2)
+                .fit(df);
+
+// alternatively, define CountVectorizerModel with a-priori vocabulary
+        CountVectorizerModel cvm = new CountVectorizerModel(new String[]{"a", "b", "c"})
+                .setInputCol("text")
+                .setOutputCol("feature");
+
+        cvModel.transform(df).show(false);
+    }
+}
